2daba37e8bcb4436d5c7605cc10982a2e009df35
Clev <105460616+clevartist@users.noreply.github.com>
2024-06-29 22:23:11 +0300

    Create MatrixIteration.cs

---
+++ C#/MatrixIteration.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Matrix {
+  static void Main() {
+    int[,] nums = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+	// ITERATE THROUGH A ROW
+    for(int i = 0; i < nums.GetLength(1); i++) {
+        Console.Write(nums[0, i] + " ");
+		/*
+  		nums[0, i]
+		0 -> row's index
+  		i -> column's index
+		*/
+    }
+
+	// ITERATE THROUGH A COLUMN
+	for(int i = 0; i < nums.GetLength(1); i++) {
+		Console.WriteLine(nums[i, 0);
+	}
+
+	// ITERATE THROUGH EVERYTHING
+	for(int i = 0; i < nums.GetLength(0); i++) {
+        for(int j = 0; j < nums.GetLength(1); j++) {
+            Console.Write(nums[i, j] + " ");
+        }
+    }
+  }
+}
